cc4c6fdb398cd9ab3983d6b6064f77101c3d3f8b
Hyunsu Joo <hs9880@gmail.com>
2022-01-15 09:51:59 +0900

    :sparkles: ADD: eslint, prettier setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import React from 'react';
+import React from 'react'
 import './styles.css'
 import image from './final_profile.png'
 import injectionSvg from './injection.svg'
-import {ClickCounter} from './ClickCouter'
+import { ClickCounter } from './ClickCouter'
 export const App = () => {
-    return (
-      <>
-            <h1>React Typescript hihi</h1>
-        <img src={image} alt="react logo" width="200" height="300"></img>
-        <img src={injectionSvg} alt="react logo"></img>
-        <ClickCounter />
-      </>
-    );
+  const hi = dkjafj
+  sldjfa
+  ljsf
+  lajsdfljasdaslkfja
+  ls
+  return (
+    <>
+      <h1>React Typescript hihi</h1>
+      <img src={image} alt="react logo" width="200" height="300"></img>
+      <img src={injectionSvg} alt="react logo"></img>
+      <ClickCounter />
+    </>
+  )
 }
